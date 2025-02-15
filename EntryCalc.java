b7822ae36495c319e4c93b456b1e8cee7f49a3fa
Lenz <lenz.neethling@gmail.com>
2025-02-15 23:22:41 +0200

    added potential profit and losses

--- EntryCalc.java
+++ EntryCalc.java
@@ -1,19 +1,20 @@
 class EntryCalc {
     public static void main(String[] args) {
 
-        if(args.length != 1 && args.length != 3) {
+        if(args.length != 1 && args.length != 4) {
             System.err.println("invalid number of arguments.");
             System.exit(0);
         }
 
         if(args[0].equals("args")) {
-            System.out.println("1. entry price  2. stoploss percentage  3. take profit percentage");
+            System.out.println("1. lot size 2. entry price  3. stoploss percentage  4. take profit percentage");
             System.exit(0);
         }
-
-        float entryPrice = getArgAsFloat(args[0]);
-        float stopPercent = getArgAsFloat(args[1])/100;
-        float takePercent = getArgAsFloat(args[2])/100;
+	
+	float lotSize = getArgAsFloat(args[0]);
+        float entryPrice = getArgAsFloat(args[1]);
+        float stopPercent = getArgAsFloat(args[2])/100;
+        float takePercent = getArgAsFloat(args[3])/100;
 
         float stopDifference = entryPrice * stopPercent;
         float takeDifference = entryPrice * takePercent;
@@ -30,6 +31,11 @@ class EntryCalc {
         System.out.println("Sell:");
         System.out.println("Stop loss: " + stopLoss);
         System.out.println("take profit: " + takeProfit);
+        System.out.println();
+
+        System.out.println("potential loss: $" + stopDifference * lotSize);
+        float potentialProfit = takeDifference - stopDifference;
+        System.out.println("potential profit: $" + potentialProfit * lotSize);
     }
 
     public static float getArgAsFloat(String arg) {
